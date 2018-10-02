7b71e4f8c28ff29bb74ca00d02970944368d8be4
John McCaffrey <john@theartboy.net>
2018-10-01 22:20:03 -0500

    Add files to repo.

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by John McCaffrey on 10/1/18.
+//  Copyright © 2018 John McCaffrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
